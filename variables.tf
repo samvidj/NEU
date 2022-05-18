ff556610f75f895fc1825a291aadf13b525c0302
samvidj <105806110+samvidj@users.noreply.github.com>
2022-05-18 13:19:15 -0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "domain_name" {
+  description = "domain name (or application name if no domain name available)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "tags for all the resources, if any"
+}
+
+variable "hosted_zone" {
+  default     = "neutest.com"
+  description = "Route53 hosted zone"
+}
+
+variable "acm_certificate_domain" {
+  default     = null
+  description = "Domain of the ACM certificate"
+}
+
+variable "price_class" {
+  default     = "PriceClass_100" // Only US,Canada,Europe
+  description = "CloudFront distribution price class"
+}
+
+variable "use_default_domain" {
+  default     = true
+  description = "Use CloudFront website address without Route53 and ACM certificate"
+}
+
+variable "upload_sample_file" {
+  default     = false
+  description = "Upload sample html file to s3 bucket"
+}
+
+# All values for the TTL are important when uploading static content that changes
+# https://stackoverflow.com/questions/67845341/cloudfront-s3-etag-possible-for-cloudfront-to-send-updated-s3-object-before-t
+variable "cloudfront_min_ttl" {
+  default     = 0
+  description = "The minimum TTL for the cloudfront cache"
+}
+
+variable "cloudfront_default_ttl" {
+  default     = 86400
+  description = "The default TTL for the cloudfront cache"
+}
+
+variable "cloudfront_max_ttl" {
+  default     = 31536000
+  description = "The maximum TTL for the cloudfront cache"
+}
